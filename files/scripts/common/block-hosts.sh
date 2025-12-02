321ea4b479de02f068aecb25aa5ada66ff4c5155
noahdotpy <noahdotpy@proton.me>
2025-12-02 21:37:06 +1000

    remove all unneccesary fluf

--- files/scripts/common/block-hosts.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-FILE_DOWNLOAD="https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/gambling-porn/hosts"
-OUTPUT_FILE="/usr/share/hosts/stevenblack-hosts.conf"
-
-mkdir -p $(dirname $OUTPUT_FILE)
-wget $FILE_DOWNLOAD -O $OUTPUT_FILE
