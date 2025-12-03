9fef2e52439a06ccde98d5fdf9b0441765eb2496
noahdotpy <noahdotpy@proton.me>
2025-12-04 07:56:05 +1000

    remove every image that not used

--- files/scripts/gnome/remove-gnome-classic.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-if [[ "$(rpm -qa | grep gnome-classic-session-xsession)" =~ "gnome-classic-session-xsession" ]]; then
-    dnf remove -y gnome-classic-session gnome-classic-session-xsession
-fi
